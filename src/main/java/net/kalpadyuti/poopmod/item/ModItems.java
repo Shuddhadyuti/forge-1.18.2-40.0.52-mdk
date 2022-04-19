76e03e8af6a324fed2afe6a4f0ffe55bbcad87f1
Shuddhadyuti <shuddhadyuti.sarkar@gmail.com>
2022-04-19 15:39:57 +0530

    Ini

---
+++ src/main/java/net/kalpadyuti/poopmod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.kalpadyuti.poopmod.item;
+
+import net.kalpadyuti.poopmod.PoopMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, PoopMod.Mod_ID);
+
+    public static final RegistryObject<Item> CITRINE = ITEMS.register("citine",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
